ec3d138ebcf2f7880bc52ce29e9842060d3affea
Joshua Horasek <joshua.horacsek@ucalgary.ca>
2022-09-21 20:53:17 -0700

    Fixing setup file

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     ],
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
-    # package_dir={"": "deep_aa"},  # Optional
+    package_dir={"": "deep_aa"},  # Optional
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
